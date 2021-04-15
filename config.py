791ba1bf7718092f5b18b4c8198af34d2aa8dd2c
patulinomods <73912095+patulinomods@users.noreply.github.com>
2021-04-15 16:11:37 -0300

    BIG UPDATE

---
+++ config.py
@@ -0,0 +1,7 @@
+BOT_TOKEN = "1794265215:AAEaZrTiJ25sh06SbF_54PKg5xlmW_ZSYzY"
+USE_WEBHOOK = False
+WEBHOOK_IP = "127.0.0.1"
+WEBHOOK_PORT = 9001
+WEBHOOK_URL = "https://domain.example.com/" + BOT_TOKEN
+CERTPATH = "/etc/certs/example.com/fullchain.cer"
+LOGLEVEL = "INFO"
